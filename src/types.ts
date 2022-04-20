f96a209bae46de9e85a2647c1d7696a6565c981f
Ioannis Mertzanis <imertz@protonmail.com>
2022-04-21 00:47:15 +0300

    Added options

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,7 @@
-export type options = {
+export type Options = {
   currency?: boolean
   ignoreDecimal?: boolean
   ignoreZeroCurrency?: boolean
+  klisi?: 'onomastiki' | 'geniki' | 'aitiatiki'
+  genos?: 'arseniko' | 'thyliko' | 'oudetero'
 }
